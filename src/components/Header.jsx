b79f0cd1daf7bedfc8d298145ae2012957d37756
OliveiraDaniel <danielosza@gmail.com>
2026-06-28 17:19:10 -0300

    style: move image to public folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
-import vinylBg from '../assets/vinyl-bg.svg'
 import '../styles/Header.css'
 
 export default function Header() {
   return (
-    <header className="header" style={{ backgroundImage: `url(${vinylBg})` }}>
+    <header className="header">
       <span className="header-title">
         COLLECTION <span className="header-title-accent">ORGANIZER</span>
       </span>
